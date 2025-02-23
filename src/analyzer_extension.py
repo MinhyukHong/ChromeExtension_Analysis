5bde6f4363bab64d6e56ee0bf072c39d610e7493
himchan02-khu <himchan02@khu.ac.kr>
2025-02-23 23:53:28 +0900

    rm batch_anlyze.py & merge in analyzer_extension.py

--- src/analyzer_extension.py
+++ src/analyzer_extension.py
@@ -95,3 +95,24 @@ def analyze_extension(zip_path, output_csv):
     print(f"Permissions: {permissions}")
 
 
+#batch_analyzer.py
+#import os
+#from analyzer_extension import analyzer_extension
+
+INPUT_FOLDER = "/home/minhyuk/Desktop/Download_extension/other"  # ZIP 파일이 있는 폴더
+OUTPUT_CSV = "extension_analysis.csv"
+
+# 모든 ZIP 파일 분석 실행
+def batch_analyze():
+    with open(OUTPUT_CSV, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["File", "API Counts", "Permissions"])
+
+    for file in os.listdir(INPUT_FOLDER):
+        if file.endswith(".zip"):
+            zip_path = os.path.join(INPUT_FOLDER, file)
+            analyze_extension(zip_path, OUTPUT_CSV)
+
+batch_analyze()
+
+
